e8ffd2c6ab02deb30236b53eb219523f9a1b48fe
Alberto <alberto.aeraph@gmail.com>
2019-07-26 02:46:02 +0000

    Create user seed

---
+++ data/users/user.ts
@@ -0,0 +1,9 @@
+import {UserContract} from "../../app/modules/core/contract/user.contract"
+
+const users : UserContract  = {
+    "name": "Alberto",
+    "email": "ricardo@gmail.com",
+    "password": "123456"
+}
+
+export = users;
