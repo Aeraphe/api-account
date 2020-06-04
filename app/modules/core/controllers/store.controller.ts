cd5a1cbd6403c779a6c582b8cf2d33acdad81105
root <root@ba67efeb5331>
2020-06-04 01:01:31 +0000

    Start StoreController

---
+++ app/modules/core/controllers/store.controller.ts
@@ -0,0 +1,12 @@
+import {StoreRepository} from '../repository/store.repository';
+
+import {Request,Response} from 'express';
+
+
+export class StoreController {
+
+    public async create(req:Request,res:Response){
+     
+    }
+    
+}
